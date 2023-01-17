a51ae261936dd23a3b0d9138e57f75d8b5607bfe
Dilshodbek <Dilshodsh97@gmail.com>
2023-01-17 19:56:13 +0500

    Add project files.

---
+++ PdpB27_Darslar/6-Dars.Enum_Struct_Generic/Startup.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PdpB27_Darslar._6_Dars.Enum_Struct_Generic
+{
+    internal class Startup
+    {
+        public static void Start()
+        {
+
+        }
+    }
+}
---
+++ PdpB27_Darslar/Program.cs
@@ -0,0 +1,11 @@
+﻿
+namespace PdpB27_Darslar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            PdpB27_Darslar._6_Dars.Enum_Struct_Generic.Startup.Start();
+        }
+    }
+}
