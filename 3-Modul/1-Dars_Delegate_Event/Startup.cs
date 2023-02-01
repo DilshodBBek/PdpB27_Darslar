8ed941c89d08f91d600b08f47b984d7ea3cd57a3
Dilshodbek <Dilshodsh97@gmail.com>
2023-02-01 18:25:09 +0500

    Add new module

---
+++ 3-Modul/1-Dars_Delegate_Event/Startup.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3_Modul._1_Dars_Delegate_Event
+{
+    internal class Startup
+    {
+        public static void Start()
+        {
+
+        }
+    }
+}
+
+
---
+++ 3-Modul/Program.cs
@@ -0,0 +1,12 @@
+﻿using _3_Modul._1_Dars_Delegate_Event;
+
+namespace _3_Modul
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Startup.Start();
+        }
+    }
+}
